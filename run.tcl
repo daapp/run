e3606fae23fc2b163a7c07952656c7b5ca92ce97
Alexander Danilov <alexander.a.danilov@gmail.com>
2017-03-19 12:51:31 +0300

    Code cleanup. Improve GUI.
    Code cleanup.
    Improve GUI.

--- run.tcl
+++ run.tcl
@@ -4,80 +4,67 @@ exec tclsh "$0" ${1+"$@"}
 
 package require Tk
 
-# Put correct path to you wish here.( /usr/bin/wish, on RedHat Linux),
-# or may be tixwish, if you don't have Tix or Img as loadable modules
-
-#
-# Alternate run command for fvwm95. Copyright(c) by Softweyr, 1997
-# All rights reserved. Distributed under GNU Public License
-#
-# Recommended style:
-# Style run  TitleIcon "",NoButton 1, NoButton 4, NoButton 6, WindowListSkip
-
-# Following line can be changed to package require Tix, or commented out
-# if you have tix or Img compilied statically.
-package require Img
-
-#
-# Few global variables which you might change
-#
-
 # Maximum size of history file
 set history_size 20
 
-# Place, where run.xpm resides
-set fvwm_icon_path /usr/X11R6/icons
-
 #list of hosts, which you use to connect via rsh (look into you .rhosts)
 set hostlist {rhine mod env amazon}
+
 # do various program require xterm
 array set xterm {
-grep 1
-sed 1
-netscape* 0
-awk 1
-tar 1
-mc 1
+    grep 1
+    sed 1
+    netscape* 0
+    awk 1
+    tar 1
+    mc 1
 }
-# This procedure creates main window. It could be safely put in main program,
-# but...
+
+
 proc layout {} {
-    global hostlist host fvwm_icon_path
-    frame .top
-    label .top.i ;#-image [image create pixmap -file $fvwm_icon_path/run.xpm]
-    label .top.l -text \
-        "Type then name of a program or script and Unix will run it for you" \
-        -wraplength 300 -justify left -pady 10
-    pack .top.i .top.l -side left
+    global hostlist host
+    frame .input
+    label .input.l -text "Command: "
+    entry .input.e -background white -textvariable command -exportselection false
+    menubutton .input.history -pady 0 -text V -menu .input.history.m -relief raised
+    menu .input.history.m
+    readhistory .input.history.m
+    pack .input.l .input.e .input.history -side left -fill y
+    pack .input.e -fill x -expand true
+
+    frame .buttons
+
+    frame .buttons.w
+    label .buttons.w.l -text "Host: " -anchor w
+    eval tk_optionMenu .buttons.w.m host localhost $hostlist
+    pack .buttons.w.l .buttons.w.m -side left
+
+    frame .buttons.s1 -padx 5
+
+    button .buttons.run -text Run -command run
+    button .buttons.cancel -text Cancel -command quit
+    button .buttons.browse -text Browse... -command browse
+    grid .buttons.w .buttons.s1 .buttons.run .buttons.cancel .buttons.browse -sticky ew -pady 5 -pady 5
+    grid columnconfigure .buttons {1 2 3} -uniform a
+
+    pack .input -fill x -expand true -padx 5 -pady 5
+    pack .buttons -fill x -padx 5 -pady 0 -expand true
+
+    bind . <Escape> quit
+    bind .input.e <Key-Return> [list .buttons.run invoke]
+    bind . <Map> {wm geometry . [winfo reqwidth .]x[winfo reqheight .]}
+
     wm title . Run
     wm positionfrom . program
-    wm geometry . +50-100
-    frame .i
-    label .i.l -text Open:
-    entry .i.e -width 40 -background white -textvariable command \
-        -exportselection false
-    bind .i.e <Key-Return> ".b.ok invoke"
-    menubutton .i.history -pady 0 -text v -menu .i.history.m -relief \
-        raised -bd 2
-    menu .i.history.m
-    readhistory .i.history.m
-    pack .i.l .i.e .i.history -side left
-    # This button does almost meaningless thing - chooses redirection of
-    # input/output to console or to /dev/null. Probably its meaning should
-    # be changed to "Run in xterm"
-    checkbutton .r -text "Run without controlling terminal" -variable term\
-        -onvalue null -offvalue console
-    frame .w
-    label .w.l -text "Run on:" -width 15 -anchor e
-    eval tk_optionMenu .w.m host localhost $hostlist
-    pack .w.l .w.m -side left
-    frame .b
-    button .b.ok -text Ok -width 8 -command run_it
-    button .b.cancel -text Cancel -width 8 -command exit
-    button .b.browse -text Browse... -width 8 -command browse
-    pack .b.browse .b.cancel .b.ok -side right
-    pack .top .i .r .w .b -fill x -expand y
-    focus .i.e
+    wm resizable . 0 0
+
+    tk app Run
+    focus -force .input.e
+}
+
+
+proc quit {} {
+    exit 0
 }
 
 #
@@ -98,7 +85,7 @@ proc browse {} {
 #
 # Runs desired command and records it in the history file
 #
-proc run_it {} {
+proc run {} {
     global command term host
     if {"$host"=="localhost" } {
         exec /bin/sh -c $command >/dev/$term </dev/$term 2>/dev/$term &
@@ -141,8 +128,5 @@ proc readhistory {menu} {
     }
 }
 
-#
-# Now the main program. Set default value for term and create window.
-#
 set term stdout
 layout
